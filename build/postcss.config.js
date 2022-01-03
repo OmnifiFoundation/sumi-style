b3a52a8c0b4e8eba7830db4fc12592ead8a6071a
フィリップ <hello+git@fillip.pro>
2022-01-03 13:03:04 +0200

    First draft of the Sumi styling.

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,27 +1,25 @@
 const fs = require('fs');
-//const { homepage, version, author }; //, packageConfig } = JSON.parse(fs.readFileSync('../package.json'));
-
 module.exports = (ctx) => {
-    const prefix = ctx.env === 'compat' ? '' : ''; //packageConfig.prefix;
+    const prefix = ctx.env === "compat" ? "" : "";
 
     return {
         map: ctx.options.map,
         parser: ctx.options.parser,
         plugins: {
-            'postcss-import': { root: ctx.file.dirname },
-            'postcss-prefixer': {
+            "postcss-import": { root: ctx.file.dirname },
+            "postcss-prefixer": {
                 prefix,
                 ignore: [/\[class\*=.*\]/],
             },
-            'postcss-preset-env': {
+            "postcss-preset-env": {
                 autoprefixer: {
                     cascade: false,
                 },
                 features: {
-                    'custom-properties': true,
+                    "custom-properties": true,
                 },
             },
-            cssnano: ctx.env === 'production' || ctx.env === 'compat' ? {} : false,
+            cssnano: ctx.env === "production" || ctx.env === "compat" ? {} : false,
         },
     };
 };
